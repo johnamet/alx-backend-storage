4a296cbf371e82bcad42896e58820f7f1195fbe4
johnny <johnametepeagboku@gmail.com>
2024-06-22 10:32:28 +0000

    Update: Task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python3
 """The script contains a Cahce class that initialises redis"""
+import functools
 import uuid
 from typing import Union, Callable, Optional
 
 import redis
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    A decorator that counts the number of times a method has been called
+    :param method: The method to be decorated
+    :return: The decorated method
+    """
+
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper function that increments the count and calls the decorated method
+        :param self: The instance of the class
+        :param args: The arguments of the method
+        :param kwargs: The keyword arguments of the method
+        :return:  The result of the decorated method
+        """
+
+        key = f'{method.__qualname__}:{self}'
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -11,6 +36,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores data in the cache
